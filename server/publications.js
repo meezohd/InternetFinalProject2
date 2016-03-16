df49d5a8befdb844156c450552aefbee8b04c012
MeezoHD <m3-m3@msn.com>
2016-03-16 11:31:22 -0500

    Connect clubs with leagues

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,13 @@
+Meteor.publish('leagues', function() {
+    return Leagues.find();
+});
+Meteor.publish('leaguePage', function(league_id) {
+    return Clubs.find({club_league_id: league_id});
+});
+
+
 Meteor.publish('clubs', function() {
-    return Clubs.find({}, {
-        sort: {_id: -1}
-    });
+    return Clubs.find();
 });
 Meteor.publish('clubPage', function(club_id) {
     return Players.find({player_club_id: club_id});
