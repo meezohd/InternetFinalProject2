3bb7b26601fb108037ccecd0737871d060af5e09
MeezoHD <m3-m3@msn.com>
2016-03-16 10:48:53 -0500

    Connect players with clubs

--- server/dataSample.js
+++ server/dataSample.js
@@ -1,8 +1,32 @@
-if (Players.find().count() === 0){
+if (Leagues.find().count() === 0){
+
+    var League1 = Leagues.insert({
+        league_name : "Liga BVAA" ,
+        league_picture : "/image/img.png" ,
+    });
+    var League2 =Leagues.insert({
+        league_name : "Serie A" ,
+        league_picture : "/image/img.png" ,
+    });
+
+
+
+    var club1 = Clubs.insert({
+        club_league_id : League1 ,
+        club_name : "Real Madrid" ,
+        club_picture : "/image/img.png" ,
+    });
+    var club2 =Clubs.insert({
+        club_league_id : League2 ,
+        club_name : "Barcelona" ,
+        club_picture : "/image/img.png" ,
+    });
+
+
 
     Players.insert({
         player_nation_id : 1 ,
-        player_club_id : 1 ,
+        player_club_id : club1 ,
         player_name : "Messi" ,
         player_birthday : "1990" ,
         player_picture : "/image/img.png" ,
@@ -14,13 +38,37 @@ if (Players.find().count() === 0){
 
     Players.insert({
         player_nation_id : 1 ,
-        player_club_id : 1 ,
+        player_club_id : club1 ,
         player_name : "Ronaldo" ,
         player_birthday : "1990" ,
         player_picture : "/image/img.png" ,
         player_position : "Attacker" ,
         player_height : "198",
         player_number : "15" ,
+        player_foot : "right"
+    });
+
+    Players.insert({
+        player_nation_id : 1 ,
+        player_club_id : club2 ,
+        player_name : "ibrahimovic" ,
+        player_birthday : "1990" ,
+        player_picture : "/image/img.png" ,
+        player_position : "Attacker" ,
+        player_height : "198",
+        player_number : "12" ,
+        player_foot : "right"
+    });
+
+    Players.insert({
+        player_nation_id : 1 ,
+        player_club_id : club2 ,
+        player_name : "Neymar" ,
+        player_birthday : "1990" ,
+        player_picture : "/image/img.png" ,
+        player_position : "Attacker" ,
+        player_height : "198",
+        player_number : "6" ,
         player_foot : "right"
     });
 }
--- server/publications.js
+++ server/publications.js
@@ -1,9 +1,16 @@
-Meteor.publish('players', function() {
-    return Players.find({}, {
+Meteor.publish('clubs', function() {
+    return Clubs.find({}, {
         sort: {_id: -1}
     });
 });
+Meteor.publish('clubPage', function(club_id) {
+    return Players.find({player_club_id: club_id});
+});
 
+
+Meteor.publish('players', function() {
+    return Players.find();
+});
 Meteor.publish('playerPage', function(id) {
     return Players.find(id);
 });
