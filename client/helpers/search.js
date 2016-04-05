d7295137f5704c0d39f69b8e6401f5d5e296b111
MeezoHD <m3-m3@msn.com>
2016-04-04 21:24:04 -0500

    done header search functionality

--- client/helpers/search.js
+++ client/helpers/search.js
@@ -6,12 +6,39 @@ Tracker.autorun(function() {
 Template.index.events({
     'keyup [type=text]': function(event, template) {
         Session.set('playersSearchQuery', event.target.value);
-    }
+    },
+    'blur [type=text]': function(event, template) {
+        setTimeout(function(){
+            Session.set('playersSearchQuery', '');
+        },1000)}
 });
+
+
 
 Template.index.helpers({
     searchResults: function() {
+        if (Session.get('playersSearchQuery'))
         return Players.search(Session.get('playersSearchQuery'));
+    },
+    playersSearchQuery: function() {
+        return Session.get('playersSearchQuery');
+    }
+});
+
+Template.header.events({
+    'keyup [type=text]': function(event, template) {
+        Session.set('playersSearchQuery', event.target.value);
+    },
+    'blur [type=text]': function(event, template) {
+        setTimeout(function(){
+            Session.set('playersSearchQuery', '');
+        },500)}
+});
+
+Template.header.helpers({
+    searchResults: function() {
+        if (Session.get('playersSearchQuery'))
+            return Players.search(Session.get('playersSearchQuery'));
     },
     playersSearchQuery: function() {
         return Session.get('playersSearchQuery');
